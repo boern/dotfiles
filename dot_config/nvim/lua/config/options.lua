f390b4b0d6b87458e96a76351ac1de044f06bc96
Boern <boern.x@gmail.com>
2026-04-13 10:51:13 +0800

    add neovim config

---
+++ dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,7 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+-- 系统剪贴板同步： 让你在 Neovim 里 y (yank) 的内容直接能通过 Cmd+V 粘贴到微信或浏览器。
+vim.opt.clipboard = "unnamedplus"
+-- 开启真彩色
+vim.opt.termguicolors = true
